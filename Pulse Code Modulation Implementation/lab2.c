16ed291b31edcc69c3f99b3afe6808e201c2a839
Chathura De Silva <97898278+chathura7357@users.noreply.github.com>
2023-08-26 19:21:34 +0530

    proceeding 2nd lab

--- Pulse Code Modulation Implementation/lab2.c
+++ Pulse Code Modulation Implementation/lab2.c
@@ -1,7 +1,7 @@
 // general purpose standard C lib
 #include <stdio.h>
 #include <stdlib.h> // stdlib includes malloc() and free()
-
+#include <math.h>
 // user-defined header files
 #include "mypcm.h"
 
@@ -9,6 +9,8 @@
 // function prototypes
 void run(asignal * inputsignal);
 
+float amplitude; /*#### This is the amplitude of the signal. 
+                 This is delcared as a separate global variable because there is no way to access amplitude inside the Quantizer() function.*/
 
 int main()
 {
@@ -16,14 +18,35 @@ int main()
     run(inputsignal);
     
     //call any other function here
-
+    amplitude = inputsignal->A; //#### Assigning value to amplitude to access it inside quantizer() function.
     free(inputsignal);
 	return 0;
 }
 
 void run(asignal * inputsignal) 
 {
-	
+    float interval;  // interval is the sampling interval
+    int encoderbits ;  // encoderbits is the number of bits used for encoding
+
+    // read input from stdin and assign to inputsignal, interval, and encoderbits.
+	scanf("%f %f %f %d %f %d", &inputsignal->A, &inputsignal->omega, &inputsignal->sigma, &inputsignal->duration, &interval, &encoderbits);
+
+    float * samples = (float *) malloc(sizeof(float) * (inputsignal->duration / interval)); // samples is the array of samples
+    sampler(samples, interval, *inputsignal); // generate samples and store in samples array.
+
+    int * pcmpulses = (int *) malloc(sizeof(int) * (inputsignal->duration / interval)); // pcmpulses is the array of quantized samples
+    quantizer(samples, pcmpulses, (int)pow(2,encoderbits)); // quantize samples and store in pcmpulses array.
+    free(samples); // free samples array
+
+    int * dsignal = (int *) malloc(sizeof(int) * (inputsignal->duration / interval) * encoderbits); // dsignal is the array of encoded samples. elements are bits.
+    encoder(pcmpulses, dsignal, encoderbits); // encode pcmpulses and store in dsignal array.
+    free(pcmpulses); // free pcmpulses array
+   
+    for (int i = 0; i < (inputsignal->duration / interval)* encoderbits; i++) {
+        // printf("%d", dsignal[i]); // print encoded samples
+    }
+    printf("\n");
+    free(dsignal); // free dsignal array becuase the only instruction is to output the array of bits.
 }
 
 
--- Pulse Code Modulation Implementation/mypcm.c
+++ Pulse Code Modulation Implementation/mypcm.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h> // includes malloc(),free()
 //Add any necessary headers here
-
+#include <math.h>
 // user-defined header files
 #include "mypcm.h" // do not modify this file
 
@@ -11,18 +11,48 @@
 
 // implementation
 float analog_signal_generator(asignal signal, int t)
-{
-	//TODO
+{																//printf("%f\n",signal.A * sin((signal.omega * t) + signal.sigma));  //uncomment for testing
+	return signal.A * sin((signal.omega * t) + signal.sigma);
 }
 void sampler(float *samples, int interval, asignal signal)
 {
-	//TODO
+	 for (int t = 0; t <= signal.duration; t=t+interval)
+	 	{															// printf("%d   ",t); //Uncomment for testing
+        samples[t/interval] =  analog_signal_generator(signal, t); // generate samples and store in samples array
+		}
 }
+
+extern float amplitude;  //#### This is the amplitude of the signal. its linked and same as the amplitude in lab2.c
+
 void quantizer(float *samples, int *pcmpulses, int levels)
 {
-	//TODO
+	for(int i = 0; i < sizeof(samples)/sizeof(float); i++)
+	{
+		if (samples[i] == amplitude){
+			pcmpulses[i] = levels - 1;
+		}
+		else{
+			pcmpulses[i] = floor(levels*(samples[i] + amplitude) / (2.0*amplitude));
+		}
+	}
 }
 void encoder(int *pcmpulses, int *dsignal, int encoderbits)
 {
-	//TODO
+	for (int i = 0; i < sizeof(dsignal)/sizeof(int); i++) {
+        int num = pcmpulses[i];
+        int index = encoderbits - 1; // Start from the rightmost digit
+        
+        // Convert decimal to binary
+        while (num > 0 && index >= 0) {
+            dsignal[i * encoderbits + index] = num % 2;
+            num /= 2;
+            index--;
+        }
+        
+        // Fill remaining digits with 0
+        while (index >= 0) {
+            dsignal[i * encoderbits + index] = 0;
+            index--;
+        }
+    }
 }
