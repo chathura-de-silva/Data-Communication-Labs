2bd1d0f4872526f9312ae6ab9281da98849b73fb
Chathura De Silva <97898278+chathura7357@users.noreply.github.com>
2023-08-26 20:54:00 +0530

    Basic test cases passed. Have to test more. Seems like failing for floats

--- Pulse Code Modulation Implementation/lab2.c
+++ Pulse Code Modulation Implementation/lab2.c
@@ -9,8 +9,6 @@
 // function prototypes
 void run(asignal * inputsignal);
 
-float amplitude; /*#### This is the amplitude of the signal. 
-                 This is delcared as a separate global variable because there is no way to access amplitude inside the Quantizer() function.*/
 
 int main()
 {
@@ -18,7 +16,6 @@ int main()
     run(inputsignal);
     
     //call any other function here
-    amplitude = inputsignal->A; //#### Assigning value to amplitude to access it inside quantizer() function.
     free(inputsignal);
 	return 0;
 }
@@ -27,23 +24,31 @@ void run(asignal * inputsignal)
 {
     float interval;  // interval is the sampling interval
     int encoderbits ;  // encoderbits is the number of bits used for encoding
-
+    int arraylength; //will change the value during execution since some arrays are different in length.
+    
     // read input from stdin and assign to inputsignal, interval, and encoderbits.
 	scanf("%f %f %f %d %f %d", &inputsignal->A, &inputsignal->omega, &inputsignal->sigma, &inputsignal->duration, &interval, &encoderbits);
 
-    float * samples = (float *) malloc(sizeof(float) * (inputsignal->duration / interval)); // samples is the array of samples
+    arraylength = (1+ inputsignal->duration / interval); // arraylength is the size of the array of samples without additional 2 elements which may specify some parameters in arrays except in the dsignal and pcmpulses. +1 is since there is a sampling point at the intial starting point.
+    
+    float * samples = (float *) malloc(sizeof(float) * (2 +arraylength)); // samples is the array of samples. It contains two addtional elements in the start. 
     sampler(samples, interval, *inputsignal); // generate samples and store in samples array.
 
-    int * pcmpulses = (int *) malloc(sizeof(int) * (inputsignal->duration / interval)); // pcmpulses is the array of quantized samples
+    samples[0] = 2+arraylength; // first element is for passing the  samples array size including the two additional parameters.
+    samples[1] = inputsignal->A; // second element is for passing the amplitude.
+
+    int * pcmpulses = (int *) malloc(sizeof(int) * (arraylength+1)); // pcmpulses is the array of quantized samples. It has a one additional element at start to pass the size of it.
     quantizer(samples, pcmpulses, (int)pow(2,encoderbits)); // quantize samples and store in pcmpulses array.
     free(samples); // free samples array
 
-    int * dsignal = (int *) malloc(sizeof(int) * (inputsignal->duration / interval) * encoderbits); // dsignal is the array of encoded samples. elements are bits.
+    pcmpulses[0] = arraylength+1; // first element is for passing the  pcmpulses array size including the one additional parameter.
+
+    int * dsignal = (int *) malloc(sizeof(int) * arraylength * encoderbits); // dsignal is the array of encoded samples. elements are bits.
     encoder(pcmpulses, dsignal, encoderbits); // encode pcmpulses and store in dsignal array.
     free(pcmpulses); // free pcmpulses array
    
-    for (int i = 0; i < (inputsignal->duration / interval)* encoderbits; i++) {
-        // printf("%d", dsignal[i]); // print encoded samples
+    for (int i = 0; i < arraylength* encoderbits; i++) {
+         printf("%d", dsignal[i]); // print encoded samples
     }
     printf("\n");
     free(dsignal); // free dsignal array becuase the only instruction is to output the array of bits.
--- Pulse Code Modulation Implementation/mypcm.c
+++ Pulse Code Modulation Implementation/mypcm.c
@@ -11,47 +11,51 @@
 
 // implementation
 float analog_signal_generator(asignal signal, int t)
-{																//printf("%f\n",signal.A * sin((signal.omega * t) + signal.sigma));  //uncomment for testing
+{															//	printf("%f\n",signal.A * sin((signal.omega * t) + signal.sigma));  //uncomment for testing
 	return signal.A * sin((signal.omega * t) + signal.sigma);
 }
 void sampler(float *samples, int interval, asignal signal)
 {
 	 for (int t = 0; t <= signal.duration; t=t+interval)
 	 	{															// printf("%d   ",t); //Uncomment for testing
-        samples[t/interval] =  analog_signal_generator(signal, t); // generate samples and store in samples array
-		}
+        samples[t/interval+2] =  analog_signal_generator(signal, t); // generate samples and store in samples array. start from 2 because the first two elements are for passing the true array size and amplitude.
+		                                                             //  printf("%f\n",samples[t/interval+2] ); // Uncomment for testing
+        }
 }
 
-extern float amplitude;  //#### This is the amplitude of the signal. its linked and same as the amplitude in lab2.c
 
 void quantizer(float *samples, int *pcmpulses, int levels)
-{
-	for(int i = 0; i < sizeof(samples)/sizeof(float); i++)
-	{
+{   
+    float amplitude = samples[1]; // second element is for passing the amplitude.
+    /*start from 2 because the first two elements are for passing the true array size and amplitude.
+     sample[0] is the true array size and sample[1] is the amplitude.*/ 
+	for(int i = 2; i < samples[0]; i++) 
+	{   
 		if (samples[i] == amplitude){
-			pcmpulses[i] = levels - 1;
+			pcmpulses[i-1] = levels - 1; //note that pcmpulses arrays first element is a special element for passing the size of the array. it contain pulses starting from second element.
 		}
 		else{
-			pcmpulses[i] = floor(levels*(samples[i] + amplitude) / (2.0*amplitude));
+			pcmpulses[i-1] = floor(levels*(samples[i] + amplitude) / (2.0*amplitude));
 		}
+        // printf("%f  - ",samples[i] ); // Uncomment for testing
+        // printf("%d\n",pcmpulses[i-1] ); // Uncomment for testing
 	}
 }
-void encoder(int *pcmpulses, int *dsignal, int encoderbits)
-{
-	for (int i = 0; i < sizeof(dsignal)/sizeof(int); i++) {
-        int num = pcmpulses[i];
-        int index = encoderbits - 1; // Start from the rightmost digit
+void encoder(int *pcmpulses, int *dsignal, int encoderbits) {
+    for (int i = 1; i < pcmpulses[0]; i++) {
+        int num = pcmpulses[i];  // Get the decimal pulse value
+        int index = (i - 1) * encoderbits + encoderbits - 1; // Calculate the starting index for binary storage
         
         // Convert decimal to binary
-        while (num > 0 && index >= 0) {
-            dsignal[i * encoderbits + index] = num % 2;
-            num /= 2;
-            index--;
+        while (num > 0 && index >= (i - 1) * encoderbits) {
+            dsignal[index] = num % 2;  // Store the binary digit
+            num /= 2;  // Divide the decimal number by 2 to get the next binary digit
+            index--;   // Move to the next position to the left
         }
         
-        // Fill remaining digits with 0
-        while (index >= 0) {
-            dsignal[i * encoderbits + index] = 0;
+        // Fill remaining digits with 0 if any
+        while (index >= (i - 1) * encoderbits) {
+            dsignal[index] = 0;  // Fill with 0
             index--;
         }
     }
